c59321d4aa06be333e7945568c2850f33344b26e
felipespp <felipesppereira@outlook.com>
2023-07-12 05:13:47 -0300

    Testes e tarefinhas...

--- src/segunda_lista/quatronotas.ts
+++ src/segunda_lista/quatronotas.ts
@@ -4,3 +4,16 @@ A - Se média for maior ou igual a 6.0, exibir "APROVADO"
 B - Se média for maior ou igual a 3.0 ou menor que 6.0 exibir "EXAME"
 C - Se média for menor que 3.0 exibir "RETIDO"
 */
+
+import prompt from 'prompt-sync'
+
+const promptSetup = prompt();
+
+const notas: number[] = [];
+
+for (let index = 1; index <= 4; index++) {
+    let numeroALer: any = Number(promptSetup('Digite uma nota:'));
+    while(isNaN(numeroALer)){
+        numeroALer = Number(promptSetup('Digite uma nota: '));
+    }
+}
