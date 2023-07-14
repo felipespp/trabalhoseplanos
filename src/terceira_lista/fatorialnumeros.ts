b046d0f780a497f0faee381758444b5a545f8830
felipespp <felipesppereira@outlook.com>
2023-07-14 13:00:34 -0300

    Alterações breves!

---
+++ src/terceira_lista/fatorialnumeros.ts
@@ -0,0 +1,7 @@
+/*
+25 - Calcule o fatorial de um numero
+*/
+
+for (let i = 1; i <= 10; i++) {
+    console.log(i * i);
+}
---
+++ src/terceira_lista/quadradodezcinquenta.ts
@@ -0,0 +1,3 @@
+for (let i = 10; i <= 50; i++) {
+    console.log(i * i)
+}
